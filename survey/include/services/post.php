71a98529c79aac80c0156e107937f7600eed6afe
vberzsin <vberzsin@web.de>
2020-11-21 16:25:59 +0100

    pst

---
+++ survey/include/services/post.php
@@ -0,0 +1,30 @@
+<?php
+
+// fixdiss
+$abs = (dirname(__FILE__));
+$abs = str_replace('/wp-content/plugins/nosuch/survey/include/services', '', $abs);
+
+require_once(sprintf('%s%s', $abs, '/wp-load.php'));
+require_once(sprintf('%s%s', $abs, '/wp-admin/includes/admin.php'));
+nocache_headers();
+
+do_action('admin_init');
+
+$action = empty( $_REQUEST['action'] ) ? '' : $_REQUEST['action'];
+
+if(!is_user_logged_in()){
+     if(empty($action)){
+          do_action('admin_post_nopriv');
+     } 
+     else{
+          do_action("admin_post_nopriv_{$action}");
+     }
+} 
+else {
+     if(empty($action)){
+          do_action('admin_post');
+     } 
+     else{
+          do_action("admin_post_{$action}");
+     }
+}
