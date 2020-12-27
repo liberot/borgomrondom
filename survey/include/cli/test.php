38297292c6e5044f95950a192eb096e8f7f3e10b
Antoine Liberot <liberot@Antoines-MBP.fritz.box>
2020-12-28 00:47:57 +0100

    cli for the import and suc

--- survey/include/cli/test.php
+++ survey/include/cli/test.php
@@ -2,7 +2,7 @@
 
 $cutt = '/\/wp-content\/plugins\/bookbuilder\/survey\/include\/cli/';
 $base = preg_replace($cutt, '', __DIR__);
-$path = sprintf('%s%s', $base, '/wp-blog-header.php');
+$path = sprintf('%s%s', $base, '/wp-load.php');
 
 require_once($path);
 
@@ -10,9 +10,11 @@ $res = init_log('cli-test', []);
 print_r($res);
 print PHP_EOL;
 
-print_r(add_base_to_chunk('t e s t'));
+print_r(add_base_to_chunk(base64_encode(' t e s t ')));
 print PHP_EOL;
 
 print_r('bye');
 print PHP_EOL;
+
+exit();
 
