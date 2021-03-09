885c95cca20cbd1f80df110f60290c628e864d62
Antoine Liberot <liberot@Antoines-MBP.fritz.box>
2021-03-09 17:49:44 +0100

    ln2br

--- survey/include2nd/views/client.php
+++ survey/include2nd/views/client.php
@@ -34,10 +34,10 @@ EOD;
      $field = bb_decorate_field_title($field);
 
      $field->title = esc_html($field->title);
-     $field->title = preg_replace('/\\\n\\\r/', '<br/>', $field->title);
+     $field->title = preg_replace('/\\\{1,}n\\\{1,}r/', '<br/>', $field->title);
 
      $field->description = esc_html($field->description);
-     $field->description = preg_replace('/\\\n\\\r/', '<br/>', $field->description);
+     $field->description = preg_replace('/\\\{1,}n\\\{1,}r/', '<br/>', $field->description);
 
      echo <<<EOD
 
